fca15c8be2203db036372b757c7c686a35ab1f95
Spiros Raptis <heptapod@posteo.eu>
2025-04-03 22:55:12 +0300

    better naming

--- backtest_trading_bot_v1_2.py
+++ backtest_trading_bot_v1_2.py
@@ -1,4 +1,4 @@
-from simplified_trading_bot_v1_2_regime_fitlers_only import (
+from simplified_trading_bot_v1_2_regime_filters_only import (
     CoinbaseService, TechnicalAnalysis, GRANULARITY, RSI_THRESHOLD,
     VOLUME_LOOKBACK, TP_PERCENT, SL_PERCENT, get_perp_product,
     get_price_precision, analyze, determine_tp_mode, classify_market_regime
